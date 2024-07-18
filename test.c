21cebf4dc747dbe83203c5bb51a99e9a93f4af48
mehrasmeydani <52368993+mehrasmeydani@users.noreply.github.com>
2024-07-18 02:34:08 +0000

    double/single quote

--- test.c
+++ test.c
@@ -1,19 +1,57 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "./srcs/get_next_line.h"
 
-int	main(void)
+int	is_sep(char *charset, char c)
 {
-	char *line[20];
-	int	fd1;
-	int	fd2;
-	int	fd3;
-	fd2 = open("abc", O_RDONLY);
-	fd3 = open("'file1'$'\n\n\n\n'", O_RDONLY);
-	printf("%d, %d, %d", fd1, fd2, fd3);
-	get_next_line(fd1, line);
-	printf("%s", line[0]);
-	// get_next_line(fd2, line);
-	// get_next_line(fd3, line);
-	// printf("%s, %s, %s", line[fd1], line[fd2], line[fd3]);
+	while (*charset)
+	{
+		if (*charset == c)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+int	count(char *str, char *charset)
+{
+	int		len_sub;
+	int		len_sep;
+	int		i;
+	int		double_q;
+	int		single_q;
+
+	len_sub = 0;
+	len_sep = 0;
+	single_q = 0;
+	double_q = 0;
+	i = 0;
+	while (str[len_sub + len_sep])
+	{
+		while (is_sep(charset, str[len_sub + len_sep]) 
+			&& str[len_sub + len_sep])
+			len_sep++;
+		if (!is_sep(charset, str[len_sub + len_sep]) 
+			&& str[len_sub + len_sep])
+			i++;
+		while ((!is_sep(charset, str[len_sub + len_sep]) 
+			&& str[len_sub + len_sep]) || (single_q % 2 == 1 || double_q % 2 == 1))
+		{
+			if (str[i] == '\"' && single_q % 2 == 0)
+			double_q++;
+			if (str[i] == '\'' && double_q % 2 == 0)
+			single_q++;
+			len_sub++;
+		}
+	}
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
+
+	printf("%d, %s, %s, %d\n",argc, argv[1], argv[2], count(argv[1], argv[2]));
 }
